492e1c2f0b9aa5a3646c959139ad07c210a8d0ab
tonywang <tonywang@yuda.tyc.edu.tw>
2021-04-14 20:21:20 +0800

    68. Creating our Item Resource using Flask_RESTful

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+
+
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
+        
+    def post(self, name):
+        item = {'name': name, 'price': 0}
+        items.append(item)
+        return item, 201
+        
+    def put(self, name):
+        return {'name': name}
+        
+    def delete(self, name):
+        return {'name': name}
+
+
+api.add_resource(Item, '/item/<string:name>')
+
+app.run(port=5000)
